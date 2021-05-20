b2d23c4d143680b1240b74e06c98699d223bed41
qttq23 <1753102@student.hcmus.edu.vn>
2021-05-21 01:41:12 +0700

    init. volume control for newfeed video and large video

---
+++ contentScript.js
@@ -0,0 +1,92 @@
+
+(()=>{
+
+	const classVolumeBtn = '.mute-icon.browse-mode';
+	const classVolumeBtn2 = '.mute-icon.one_column';
+	const classVideoPlayer = '.video-player'
+	const idExtVolumeDiv = 'extension-volume-div';
+	const classExtVolumeInput = 'extension-volume-input';
+
+	let replaceVolume = ()=>{
+
+		let listBtnVolume = [
+		...document.querySelectorAll(classVolumeBtn),
+		...document.querySelectorAll(classVolumeBtn2),
+		];
+		console.log('found: ', listBtnVolume.length);
+
+		for(let btnVolume of listBtnVolume) {
+			if(btnVolume.id != idExtVolumeDiv) {
+				if(btnVolume.style.display != 'none') {
+					
+					console.log('about to hde default volume');
+
+					// hide default volume
+					btnVolume.style.display = 'none';
+
+					// show extension volume
+					if(btnVolume.parentNode.getElementsByClassName(classExtVolumeInput).length > 0){
+						return;
+					}
+
+					console.log('about to show extension-volume');
+
+					let currentVolumeRate = document.querySelector(classVideoPlayer).volume;
+					const html = `
+					<div class="${btnVolume.className} cls-ext-vol" id="${idExtVolumeDiv}">
+
+					<input class="${classExtVolumeInput}"
+					style="position: absolute;
+					top: 40%;
+					transform: rotate(270deg);" 
+					type="range" name="vol" min="0" max="100" value="${currentVolumeRate * 100}">
+
+					</div>
+					`;
+					var div = document.createElement('div');
+					div.innerHTML = html.trim();
+
+
+					// btnVolume.parentNode.innerHTML += html;
+					btnVolume.parentNode.appendChild(div.firstChild);
+
+
+
+
+
+				}
+			}
+		}
+	};
+
+
+	let loopReplaceVolume = ()=>{
+
+		setTimeout(()=>{
+			replaceVolume();
+			setTimeout(loopReplaceVolume, 100);
+		}, 100); 
+
+	};
+	loopReplaceVolume();
+
+
+	let addHandler = ()=>{
+		setTimeout(()=>{
+
+			let listVol = document.getElementsByClassName(classExtVolumeInput);
+			for(let vol of listVol) {
+				vol.oninput = (event)=>{
+					let players = document.querySelectorAll(classVideoPlayer);
+					for(let player of players) {
+						player.volume = event.target.value/ event.target.max;
+					}
+				};
+			}
+			addHandler();
+		}, 200); 
+	};
+	addHandler();
+
+
+})();
---
+++ readme.md
@@ -0,0 +1,4 @@
+https://stackoverflow.com/questions/15935837/how-to-display-a-range-input-slider-vertically
+https://stackoverflow.com/questions/21422337/append-element-as-sibling-after-element
+https://stackoverflow.com/questions/494143/creating-a-new-dom-element-from-an-html-string-using-built-in-dom-methods-or-pro
+https://stackoverflow.com/questions/7815374/get-element-inside-element-by-class-and-id-javascript
